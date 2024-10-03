dd5c0ef601db681d4638df2153894bc53f784c76
Opeyemi Omotayo <96225514+Opeyemi-Omotayo@users.noreply.github.com>
2024-10-03 22:20:04 +0100

    feat: context

---
+++ src/context/EditorContext.tsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useState, ReactNode, Dispatch, SetStateAction } from "react";
+
+interface EditorContextProps {
+  title: string;
+  setTitle: (title: string) => void;
+  content: string;
+  setContent: (content: string) => void;
+  showEmbed: boolean;
+  toggleEmbeds: () => void;
+  image: string | null;
+  setImage:Dispatch<SetStateAction<string | null>>;
+}
+
+interface EditorProviderProps {
+  children: ReactNode;
+}
+
+const EditorContext = createContext<EditorContextProps | undefined>(undefined);
+
+export const EditorProvider: React.FC<EditorProviderProps> = ({ children }) => {
+  const [title, setTitle] = useState<string>("Add post title");
+  const [content, setContent] = useState<string>("");
+  const [showEmbed, setShowEmbed] = useState<boolean>(false);
+  const [image, setImage] = useState<string | null>(null); 
+
+  const toggleEmbeds = () => {
+    setShowEmbed((prev) => !prev);
+  };
+
+  return (
+    <EditorContext.Provider value={{ title, setTitle, content, setContent, toggleEmbeds, showEmbed, image, setImage }}>
+      {children}
+    </EditorContext.Provider>
+  );
+};
+
+export const useEditor = (): EditorContextProps => {
+  const context = useContext(EditorContext);
+
+  if (!context) {
+    throw new Error(
+      "useEditor must be called within a component that is wrapped by an EditorProvider"
+    );
+  }
+
+  return context;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { EditorProvider } from "./context/EditorContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <EditorProvider>
+      <App />
+    </EditorProvider>
+  </StrictMode>
+);
